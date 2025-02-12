bebac4c6e7537d9cf780977be324f485dd915b9e
Divya-kaushal5150 <dk5150@nyu.edu>
2025-02-12 18:05:55 -0500

    Add files via upload

--- webServer.py
+++ webServer.py
@@ -3,13 +3,13 @@ from socket import *
 # In order to terminate the program
 import sys
 
-
+server='localhost'
 
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   
   #Prepare a server socket
-  serverSocket.bind(('localhost', port))
+  serverSocket.bind((server, port))
   
   #Fill in start
   #Enable server to listen for incoming connections (max 1 connection)
